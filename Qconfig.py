1620f1cad412a87dbe0dd03c2d85645717c95afb
octophi <joey.li@duke.edu>
2018-06-29 15:33:30 -0400

    swapped qconfig files

---
+++ Qconfig.py
@@ -0,0 +1,9 @@
+APItoken = 'c5295149c98aeb9a1e00e471fd230ced2b05078d5555fe476b29d9da616a01ab1b9d662ef087ba1c5d47b7f6efdc84e1cdeff37911cd034f6e61de11da36f9f6'
+
+config = {
+    'url': 'https://quantumexperience.ng.bluemix.net/api',
+    # The following should only be needed for IBM Q users.
+    'hub': 'MY_HUB',
+    'group': 'MY_GROUP',
+    'project': 'MY_PROJECT'
+}
